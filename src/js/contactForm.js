fa6a601755041fa5483efa36ac3cfef2927feb5a
Satenik <saten914@gmail.com>
2025-03-06 21:51:14 +0100

    feat: Se añadimos un evento que se activará cuando el usuario envíe el formulario

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -2,3 +2,7 @@ export function initContactForm() {
     const form = document.querySelector('.contact-form form')
     if(!form) return; 
 }
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+})
